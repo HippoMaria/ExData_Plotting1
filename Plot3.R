12302d12ddf5319f00fa733ccc9574bb5d541821
HippoMaria <73005074+HippoMaria@users.noreply.github.com>
2020-11-10 13:36:49 -0800

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -9,23 +9,9 @@ original$date2<- as.Date(original$Date, "%d/%m/%Y")
 
 use<-filter(original, date2=="2007-02-01"|date2=="2007-02-02")
 
-# Plot 1
-
-png(file="Plot1.png", width=480, height=480)
-hist(use$Global_active_power, col="red", xlab="Global Active Power (kilowatt)", ylab="Frequency", main="Global Active Power")
-dev.off()
-
-# Plot 2
 
 use$datetime<-paste(use$date2,use$Time, sep=" ")
 use$DT<-as.POSIXct(use$datetime)
-
-png(file="Plot2.png", width=480, height=480)
-with(use, {
-  plot(Global_active_power~DT, type="l",
-       ylab="Global Active Power (kilowatts)", xlab="")
-})
-dev.off()
 
 # Plot 3
 
